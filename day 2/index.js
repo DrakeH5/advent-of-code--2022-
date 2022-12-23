2c119443a8392d4f5ef7ee5388de6716011b9dea
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-12-23 14:16:54 -0600

    day 2 part 2

--- day 2/index.js
+++ day 2/index.js
@@ -4,7 +4,7 @@ conversions = {
     "B": "Y",
     "C": "Z"
 }
-var points = {"X":1,"Y":2,"Z":3}
+var points = {"X":1,"Y":2,"Z":3, "A":1, "B":2,"C":3}
 var total = 0;
 fs.readFile('./input.txt', 'utf8', function (err,data) {
   if (err) {
@@ -14,8 +14,29 @@ fs.readFile('./input.txt', 'utf8', function (err,data) {
     row = i*4;
     letter1 = data[row]
     letter2 = data[row+2]
+
+    if(letter2=="X"){
+        if(letter1=="A"){
+            letter2="Z"
+        } else if(letter1=="B"){
+            letter2="X"
+        } else if(letter1=="C"){
+            letter2="Y"
+        }
+    } else if(letter2=="Y"){
+        letter2 = letter1
+    } else if(letter2=="Z"){
+        if(letter1=="A"){
+            letter2="Y"
+        } else if(letter1=="B"){
+            letter2="Z"
+        } else if(letter1=="C"){
+            letter2="X"
+        }
+    }
+
     total+=points[letter2]
-    if(letter2 == conversions[letter1]){//tie
+    if(letter2 == letter1){//tie
         total+=3
     } else {
         if(letter1=="A"){
